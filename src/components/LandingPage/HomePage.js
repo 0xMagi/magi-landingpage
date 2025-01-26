2ee793793e8c5c866fc97c162dba98ff6ad904c7
Esdras Gonçalves <magiblockchain@gmail.com>
2025-01-26 13:18:06 -0300

    Update HomePage.js

--- src/components/LandingPage/HomePage.js
+++ src/components/LandingPage/HomePage.js
@@ -1,5 +1,7 @@
 import React from "react";
-import headerRight from "../../assets/header-right.png";
-import { makeStyles, Button, Typography } from "@material-ui/core";
-;
+
+const HomePage = () => {
+  return <div></div>;
+};
+
 export default HomePage;
